f9409cafe01ee22b48c63427d323ca4e3a0903cd
Pranav Neti <pranav.neti108@gmail.com>
2024-02-06 10:23:40 -0800

    Modified variables to follow the right call stack, & sizing

--- IK Simulator.py
+++ IK Simulator.py
@@ -24,14 +24,14 @@ ampDist = 34
 
 # robot variables
 
-shoulderJoint = [295, 390]
-sholderJointToGearRadius = 34
-wristJoint = [295, 140]
-
 shoulderAngle = 90
 wristAngle = 90
 
 armLength = 250
+
+shoulderJoint = [windowWidth / 2.0 - 105, windowHeight-210]
+sholderJointToGearRadius = 34
+wristJoint = [windowWidth / 2.0 - 105, shoulderJoint[1] - armLength]
 
 L2x = 32.8
 #L2x = 52.8
@@ -57,7 +57,7 @@ def calculateIntakeWristAngle(curwristAngle):
     intakeEndToShoulderXOffset = intakeTop[0] - shoulderJoint[0]
 
     if(intakeEndToShoulderXOffset >  365):
-        desiredWristToIntake = 660 - wristJoint[0]
+        desiredWristToIntake = windowWidth / 2.0 + 260 - wristJoint[0]
         intakeWristAngle = -math.acos(desiredWristToIntake / L3) * 180.0 / math.pi
         theta4calc = intakeWristAngle - thetaL3 + 180
         wristAnglecalc = 180 - theta4calc + shoulderAngle
@@ -82,7 +82,6 @@ while run:
     if(keys[pygame.K_a]):
         shoulderAngle += 1
         
-    
     if(keys[pygame.K_d]):
         shoulderAngle -= 1
     
@@ -118,31 +117,31 @@ while run:
     intakeTop[0] = wristJoint[0] + L3 * math.cos(absThetaL3 * math.pi / 180.0)
     intakeTop[1] = wristJoint[1] - L3 * math.sin(absThetaL3 * math.pi / 180.0)
 
-    intakeBottom[0] = wristJoint[0] + L3x * math.cos( (theta4-180) * math.pi / 180.0)
+    intakeBottom[0] = wristJoint[0] + L3x * math.cos( (theta4-180) * math.pi / 180.0)      
     intakeBottom[1] = wristJoint[1] - L3x * math.sin( (theta4-180) * math.pi / 180.0)
 
 
     # Space Limit
-    pygame.draw.rect(window, (0,0,0), ((140, 120), (520, 480)), 3)
+    pygame.draw.rect(window, (0,0,0), ((windowWidth / 2.0 - 260, windowHeight - 480), (520, 480)), 3)
 
     # Amp
-    #pygame.draw.rect(window, (0,200,200), ((196, 600-ampTop), (ampDist, ampTop-ampBottom)), 3)
-    #pygame.draw.rect(window, (0,200,200), ((570, 600-ampTop), (ampDist, ampTop-ampBottom)), 3)
+    #pygame.draw.rect(window, (0,200,200), ((windowWidth / 2.0 - 204, windowHeight-ampTop), (ampDist, ampTop-ampBottom)), 3)
+    #pygame.draw.rect(window, (0,200,200), ((windowWidth / 2.0 + 170, windowHeight-ampTop), (ampDist, ampTop-ampBottom)), 3)
 
     # Source
-    #pygame.draw.line(window, (0, 200, 200), (570, 600-sourceHighHeight), (570, 600-sourceLowHeight), 3)
-    #pygame.draw.line(window, (0, 200, 200), (570, 600-sourceHighHeight), (690, 600-sourceHighHeight-156), 3)
-    #pygame.draw.line(window, (0, 200, 200), (570, 600-sourceLowHeight), (690, 600-sourceLowHeight-156), 3)
-    #pygame.draw.line(window, (0, 200, 200), (690, 600-sourceHighHeight-156), (690, 600-sourceLowHeight-156), 3)
+    #pygame.draw.line(window, (0, 200, 200), (windowWidth / 2.0 + 170, windowHeight-sourceHighHeight), (windowWidth / 2.0 + 170, windowHeight-sourceLowHeight), 3)
+    #pygame.draw.line(window, (0, 200, 200), (windowWidth / 2.0 + 170, windowHeight-sourceHighHeight), (windowWidth / 2.0 + 290, windowHeight-sourceHighHeight-156), 3)
+    #pygame.draw.line(window, (0, 200, 200), (windowWidth / 2.0 + 170, windowHeight-sourceLowHeight), (windowWidth / 2.0 + 290, windowHeight-sourceLowHeight-156), 3)
+    #pygame.draw.line(window, (0, 200, 200), (windowWidth / 2.0 + 290, windowHeight-sourceHighHeight-156), (windowWidth / 2.0 + 290, windowHeight-sourceLowHeight-156), 3)
 
     # Ring
-    #pygame.draw.rect(window, (255,127,0), ((600, 600-ringHeight), (ringLength, ringHeight)), 3)  
+    pygame.draw.rect(window, (255,127,0), ((windowWidth / 2.0 + 200, windowHeight-ringHeight), (ringLength, ringHeight)), 3)  
  
     # Bumpers
-    pygame.draw.rect(window, (181,148,15), ((230, 542.5), (340, 57.5)), 3)
+    pygame.draw.rect(window, (181,148,15), ((windowWidth / 2.0 - 170, windowHeight - 57.5), (340, 57.5)), 3)
 
     # base to shoulder
-    pygame.draw.line(window, (160, 32, 240), shoulderJoint, (295, 580), 3)
+    pygame.draw.line(window, (160, 32, 240), shoulderJoint, (shoulderJoint[0], windowHeight-20), 3)
 
     # shoulder radius
     pygame.draw.circle(window, (32,32,32), shoulderJoint, sholderJointToGearRadius, 3)
@@ -162,11 +161,7 @@ while run:
     initialPoint = [ ( shooterTop[0] + shooterBottom[0] ) / 2.0 , ( shooterTop[1] + shooterBottom[1] ) / 2.0]
     finalPoint = [ 1000 * math.cos(theta4 * math.pi / 180.0) + initialPoint[0] , -1000 * math.sin(theta4 * math.pi / 180.0) + initialPoint[1]]
 
-    pygame.draw.line(window, (255, 128, 0), initialPoint, finalPoint, 3)
-    
-    
-    
-
+    pygame.draw.line(window, (255, 128, 0), initialPoint, finalPoint, 3)  
 
     pygame.display.flip()
     clock.tick(50)
